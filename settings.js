50ed4197bcd325806e94c5f77fcbedf413f86981
xDilo694 <iinimaaa4@gmail.com>
2025-05-23 14:03:23 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'DILO MD', // Your name
-  ownerNumber: '94775896260', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '+94775896260', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
